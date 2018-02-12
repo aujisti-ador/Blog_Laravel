2cf0f94c9308578360fec1ba836fdcab3d5c089e
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-02-12 13:16:52 +0600

    db link added

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -56,33 +56,33 @@
                             <a href="#"><i class="halflings-icon cog"></i></a>
                         </div>
                         <h2>Login to your account</h2>
-                        <form class="form-horizontal" action="{{URL::to('admin_login')}}" method="post">
-                            <fieldset>
+                        {!! Form::open(['url' => 'admin_login','method'=>'post']) !!}
+                        <fieldset>
 
-                                <div class="input-prepend" title="Username">
-                                    <span class="add-on"><i class="halflings-icon user"></i></span>
-                                    <input class="input-large span10" name="username" id="username" type="text" placeholder="type username"/>
-                                </div>
-                                <div class="clearfix"></div>
+                            <div class="input-prepend" title="Username">
+                                <span class="add-on"><i class="halflings-icon user"></i></span>
+                                <input class="input-large span10" name="username" id="username" type="text" placeholder="type username"/>
+                            </div>
+                            <div class="clearfix"></div>
 
-                                <div class="input-prepend" title="Password">
-                                    <span class="add-on"><i class="halflings-icon lock"></i></span>
-                                    <input class="input-large span10" name="password" id="password" type="password" placeholder="type password"/>
-                                </div>
-                                <div class="clearfix"></div>
+                            <div class="input-prepend" title="Password">
+                                <span class="add-on"><i class="halflings-icon lock"></i></span>
+                                <input class="input-large span10" name="password" id="password" type="password" placeholder="type password"/>
+                            </div>
+                            <div class="clearfix"></div>
 
-                                <label class="remember" for="remember"><input type="checkbox" id="remember" />Remember me</label>
+                            <label class="remember" for="remember"><input type="checkbox" id="remember" />Remember me</label>
 
-                                <div class="button-login">	
-                                    <button type="submit" class="btn btn-primary">Login</button>
-                                </div>
-                                <div class="clearfix"></div>
-                        </form>
-                        <hr>
-                        <h3>Forgot Password?</h3>
-                        <p>
-                            No problem, <a href="#">click here</a> to get a new password.
-                        </p>	
+                            <div class="button-login">	
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                            <div class="clearfix"></div>
+                        {!! Form::close() !!}
+                            <hr>
+                            <h3>Forgot Password?</h3>
+                            <p>
+                                No problem, <a href="#">click here</a> to get a new password.
+                            </p>	
                     </div><!--/span-->
                 </div><!--/row-->
 
