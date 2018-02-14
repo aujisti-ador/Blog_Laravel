1d1019d8ff2a7de3d41ff7bff23372f854808a10
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-02-14 23:51:01 +0600

    form updated

--- resources/views/admin/pages/add_category.blade.php
+++ resources/views/admin/pages/add_category.blade.php
@@ -27,29 +27,26 @@
             <form class="form-horizontal">
                 <fieldset>
                     <div class="control-group">
-                        <label class="control-label" for="typeahead">Auto complete </label>
+                        <label class="control-label" for="typeahead">Category Name</label>
                         <div class="controls">
-                            <input type="text" class="span6 typeahead" id="typeahead"  data-provide="typeahead" data-items="4" data-source='["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]'>
-                            <p class="help-block">Start typing to activate auto complete!</p>
+                            <input type="text" class="span6 typeahead" id="typeahead">
                         </div>
                     </div>
-                    <div class="control-group">
-                        <label class="control-label" for="date01">Date input</label>
-                        <div class="controls">
-                            <input type="text" class="input-xlarge datepicker" id="date01" value="02/16/12">
-                        </div>
-                    </div>
-
-                    <div class="control-group">
-                        <label class="control-label" for="fileInput">File input</label>
-                        <div class="controls">
-                            <input class="input-file uniform_on" id="fileInput" type="file">
-                        </div>
-                    </div>          
+                                               
                     <div class="control-group hidden-phone">
-                        <label class="control-label" for="textarea2">Textarea WYSIWYG</label>
+                        <label class="control-label" for="textarea2">Category Description</label>
                         <div class="controls">
                             <textarea class="cleditor" id="textarea2" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="typeahead">Publication Status</label>
+                        <div class="controls">
+                            <select name="publication_status">
+                                <option>-----Publication Status-----</option>
+                                <option>Published</option>
+                                <option>Unpublished</option>
+                            </select>
                         </div>
                     </div>
                     <div class="form-actions">
